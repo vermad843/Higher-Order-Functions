20f3fc0889170da07f140144418eeece1bc79619
vermad843 <vishnuverma7524@gmail.com>
2019-10-24 18:58:01 +0530

    using forEach

--- higher-order-functions/excercise.js
+++ higher-order-functions/excercise.js
@@ -3,3 +3,21 @@
 // an argument ,or return a function as a result.
 
 
+//forEach() is a higher order function 
+// for Loops  can be replaced with forEach. 
+
+const numbers = [1, 2, 3, 4, 5];
+
+// log all the numbers in the array
+
+// with a for loop 
+
+for (let i = 0; i < numbers.length; i++) {
+    const number = numbers[i];
+  console.log(number);  
+}
+
+
+numbers.forEach((number) => {
+    console.log(number);
+})
