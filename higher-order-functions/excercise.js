bf254f15b284177a218029ba6ac158c0ddf07f71
vermad843 <vishnuverma7524@gmail.com>
2019-10-24 19:13:55 +0530

    flavors problem

--- higher-order-functions/excercise.js
+++ higher-order-functions/excercise.js
@@ -26,3 +26,33 @@ for (let i = 0; i < numbers.length; i++) {
 // one liner :
 
 numbers.forEach((number) => console.log(number));
+
+
+
+// convert the following for loops to a forEach 
+
+
+// create the array 
+
+const flavors = ['chocolate',
+                  'ginger',
+                  'carrot',
+                  'coffee',
+                  'walnut',
+                  'banana'];
+                
+for (let i = 0; i <flavors.length; i++) {
+    console.log('I like ' + flavors[i] + ' cake');
+}
+
+
+flavors.forEach((flavor, i) => {
+console.log(`I like ${flavor} cake`) 
+})
+
+// I like chocolate cake
+// I like ginger cake
+// I like carrot cake
+// I like coffee cake
+// I like walnut cake
+// I like banana cake
