2015413eeb95a3187c810b081fb77afb10200231
vermad843 <vishnuverma7524@gmail.com>
2019-10-24 15:40:25 +0530

    fullName problem

--- Map.js
+++ Map.js
@@ -43,3 +43,38 @@ const firstThreeLetters = days.map((day) => {
 console.log(firstThreeLetters);
 
 // expected output :[ 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat' ]
+
+
+
+const people = [{
+   first_name : 'Vishnu',
+   last_name : 'Verma', 
+}, {
+   first_name : 'Logan',
+   last_name : 'Verma', 
+}, {
+   first_name : 'Jane',
+   last_name : 'Verma', 
+}, {
+   first_name : 'XYZ',
+   last_name : 'Verma', 
+}];
+
+//create a new array with the string full name of 
+// each person 
+
+
+// const fullName = people.map((person) => {
+//     return person.first_name + ' ' + person.last_name;
+// })
+
+// console.log(fullName);
+
+
+const fullName = people.map((person) => {
+    return `${person.first_name} ${person.last_name}`;
+})
+
+console.log(fullName);
+
+// [ 'Vishnu Verma', 'Logan Verma', 'Jane Verma', 'XYZ Verma' ]
