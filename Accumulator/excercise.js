5c4a8cf85e84dde5224599785e9b17df2955ac16
vermad843 <vishnuverma7524@gmail.com>
2019-10-24 19:59:20 +0530

    Accumulator pattern

---
+++ Accumulator/excercise.js
@@ -0,0 +1,17 @@
+/*
+The Accumulator Pattern 
+
+A common programming "pattern" is to iterate over
+an array,accumulating a value as we go. This pattern
+is commonly used to sum the numbers in an array,
+or to build a new array based on existing array.
+
+ 3 parts of the accumulator pattern: 
+
+1. initialize an 'accumulator' variable to an initial
+   value
+2. iterating over an array
+3. updating the accumulator variable on each iteration
+
+
+*/
