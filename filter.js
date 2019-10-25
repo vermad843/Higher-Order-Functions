d63c7c326a094291dc2f9df9733d1bd24b9f7132
vermad843 <vishnuverma7524@gmail.com>
2019-10-25 09:18:29 +0530

    filter

---
+++ filter.js
@@ -0,0 +1,25 @@
+// filter is a higher order function 
+// filter should either return true or false
+// it will return the array.array returned by 
+// filter may not have the  same length like the 
+// previous array
+
+// forEach
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8 ,9, 10];
+// let evenNumbers = [];
+
+// numbers.forEach((number) => {
+//     if(number % 2 == 0) {
+//         evenNumbers.push(number);
+//     }
+// });
+
+
+// filter()
+
+let evenNumbers = numbers.filter((number) => {
+    return number % 2 == 0;
+})
+
+console.log(evenNumbers);
